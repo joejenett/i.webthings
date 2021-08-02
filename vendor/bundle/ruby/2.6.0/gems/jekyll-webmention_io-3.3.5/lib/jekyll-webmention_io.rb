3b031c929fcb3a2dd92b141412d9c0620450cab9
joejenett <52048847+joejenett@users.noreply.github.com>
2021-08-02 13:26:09 -0400

    sending to local backup

--- vendor/bundle/ruby/2.6.0/gems/jekyll-webmention_io-3.3.5/lib/jekyll-webmention_io.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: false
-
-require "jekyll"
-require "jekyll/webmention_io"
-
-Jekyll::Hooks.register :site, :after_init do |site|
-  Jekyll::WebmentionIO.bootstrap(site)
-end
--- vendor/bundle/ruby/2.6.0/gems/jekyll-webmention_io-3.3.5/lib/jekyll/webmention_io/version.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module Jekyll
-  module WebmentionIO
-    VERSION = "3.3.5"
-  end
-end
