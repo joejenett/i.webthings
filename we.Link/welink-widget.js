7349bb81d049c413521c7abc210ee57af1af7bb0
J R Jenett <52048847+joejenett@users.noreply.github.com>
2022-11-09 17:25:03 -0500

    Update welink-widget.js

--- we.Link/welink-widget.js
+++ we.Link/welink-widget.js
@@ -42,8 +42,8 @@ function randomSite() {
 	}
 
 	tag.insertAdjacentHTML('afterbegin', `
-	<div style="text-align:center;">
-	<div id='welink' style='text-align:center;border:1px solid #014678;width:56px;'>
+	<div id='welink' style="display:flex;justify-content:center;">
+	<div style='border:1px solid #014678;width:56px;'>
 	<a href="#" title="random member" onclick="randomSite()"><img src="https://iwebthings.jenett.org//we.Link/welink.png" width="56" alt="" /></a><br />
 	<a href='${indexPage}' title="homepage"><img src="https://iwebthings.jenett.org//we.Link/index.png" width="56" alt="" /></a>
 	</div>
